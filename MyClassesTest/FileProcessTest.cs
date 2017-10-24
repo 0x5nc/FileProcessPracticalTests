ec012312efb091c4414060e85953db5fe6d9da45
0x5nc <slim.essency@gmail.com>
2017-10-24 21:29:38 +0200

    Avoiding Hard-Coding in Unit Testing

--- MyClassesTest/FileProcessTest.cs
+++ MyClassesTest/FileProcessTest.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyClasses;
+using System.Configuration;
+using System.IO;
 
 namespace MyClassesTest
 {
     [TestClass]
     public class FileProcessTest
     {
+        private const string _badFileName = @"C:\RandomFileToForceError";
+        private string _goodFileName;
+
+        public TestContext TestContext { get; set; }
 
         /// <summary>
         /// Stub methods to fill later
@@ -17,9 +23,16 @@ namespace MyClassesTest
             FileProcess fp = new FileProcess();
             bool fromCall;
 
-            fromCall = fp.FileExists(@"C:\Windows\Regedit.exe");
+            SetGoodFileName();
+            TestContext.WriteLine("Creating the File " + _goodFileName);
+            File.AppendAllText(_goodFileName, "test string");
 
+            TestContext.WriteLine("Creating the File " + _goodFileName);
+            fromCall = fp.FileExists(_goodFileName);
             Assert.IsTrue(fromCall);
+
+            TestContext.WriteLine("Deleting the File " + _goodFileName);
+            File.Delete(_goodFileName);
         }
 
         [TestMethod]
@@ -28,7 +41,7 @@ namespace MyClassesTest
             FileProcess fp = new FileProcess();
             bool fromCall;
 
-            fromCall = fp.FileExists(@"C:\RandomPath.exe");
+            fromCall = fp.FileExists(_badFileName);
 
             Assert.IsFalse(fromCall);
         }
@@ -58,6 +71,16 @@ namespace MyClassesTest
             }
 
             Assert.Fail("Call to FileExists did not throw the expected exception");
+        }
+
+        private void SetGoodFileName()
+        {
+            _goodFileName = ConfigurationManager.AppSettings["GoodFileName"];
+            if (_goodFileName.Contains("[AppPath]"))
+            {
+                _goodFileName = _goodFileName.Replace("[AppPath]",
+                       Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            }
         }
     }
 }
