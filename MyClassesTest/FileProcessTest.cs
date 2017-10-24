4c2aa41d820153264487607fe0da0878fa32a05f
0x5nc <slim.essency@gmail.com>
2017-10-24 20:08:54 +0200

    Add project files.

---
+++ MyClassesTest/FileProcessTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+
+        /// <summary>
+        /// Stub methods to fill later
+        /// </summary>
+        [TestMethod]
+        public void FileNameDoesExist()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists(@"C:\Windows\Regedit.exe");
+
+            Assert.IsTrue(fromCall);
+        }
+
+        [TestMethod]
+        public void FileNameDoesNotExist()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists(@"C:\RandomPath.exe");
+
+            Assert.IsFalse(fromCall);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FileNameIsNullOrEmpty_ThrowArgumentNullException()
+        {
+            FileProcess fp = new FileProcess();
+
+            fp.FileExists("");
+        }
+
+        [TestMethod]
+        public void FileNameIsNullOrEmpty_ThrowArgumentNullException_UsingTryCatch()
+        {
+            FileProcess fp = new FileProcess();
+
+            try
+            {
+                fp.FileExists("");
+            }
+            catch(ArgumentNullException)
+            {
+                //The test was a success
+                return;
+            }
+
+            Assert.Fail("Call to FileExists did not throw the expected exception");
+        }
+    }
+}
